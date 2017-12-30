f6f2c9e0af67b3e0f1c0967680992b5ebf4fe5d4
RaulCM <raulcanomontero@hotmail.com>
2017-12-30 15:30:52 +0100

    Implementada funcionalidad para pasar la primera prueba

---
+++ Union.java
@@ -0,0 +1,13 @@
+package union;
+
+import java.util.*;
+public class Union {
+
+	public static Vector union (Vector a, Vector b){
+		Vector v = new Vector();
+		if(a==null || b== null){
+			throw new NullPointerException();
+		}
+		return v;
+	}
+}
